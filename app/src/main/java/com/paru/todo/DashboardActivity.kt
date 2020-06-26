d74bfcc9ad9112d921ba09d6315dea6bb11ed1fb
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-26 20:53:54 +0530

    dashboard activity created

--- app/src/main/java/com/paru/todo/DashboardActivity.kt
+++ app/src/main/java/com/paru/todo/DashboardActivity.kt
@@ -1,12 +1,83 @@
 package com.paru.todo
 
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.paru.todo.DTO.ToDo
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.dialog_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
+
+    lateinit var dbHandler:DBHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        setSupportActionBar(dashboard_toolbar)
+        title="Dashboard"
+
+
+        dbHandler= DBHandler(this)
+        rv_dashboard.layoutManager=LinearLayoutManager(this)
+
+        fab_dashboard.setOnClickListener{
+            val dialog:AlertDialog.Builder=AlertDialog.Builder(this)
+            val view=layoutInflater.inflate(R.layout.dialog_dashboard,null)
+            val toDoName=view.findViewById<EditText>(R.id.ev_todo)
+            dialog.setView(view)
+            dialog.setPositiveButton("Add") { _: DialogInterface, _: Int ->
+                if (toDoName.text.isNotEmpty()) {
+                    val toDo = ToDo()
+                    toDo.name = toDoName.text.toString()
+                    dbHandler.addToDo(toDo)
+                    refreshList()
+                }
+            }
+            dialog.setNegativeButton("Cancel") { _: DialogInterface, _: Int ->
+
+            }
+            dialog.show()
+        }
+    }
+
+    override fun onResume() {
+        refreshList()
+        super.onResume()
+    }
+
+    private fun refreshList(){
+        rv_dashboard.adapter=DashboardAdapter(this,dbHandler.getToDo())
+    }
+
+    class DashboardAdapter(val context: Context,val list:MutableList<ToDo>): RecyclerView.Adapter<DashboardAdapter.ViewHolder>()
+    {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(context).inflate(R.layout.rv_dashboard_singlerow,parent,false))
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.toDoName.text=list[position].name
+
+        }
+
+        class ViewHolder(v:View):RecyclerView.ViewHolder(v)
+        {
+            val toDoName:TextView=v.findViewById(R.id.tv_todo_name)
+        }
     }
 }
