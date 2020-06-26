33370d8155239f77db133b6e6919f6c38812c38e
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-26 18:37:29 +0530

    dashboard page created

--- app/src/main/java/com/paru/todo/DashboardActivity.kt
+++ app/src/main/java/com/paru/todo/DashboardActivity.kt
@@ -3,10 +3,10 @@ package com.paru.todo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard)
     }
 }
